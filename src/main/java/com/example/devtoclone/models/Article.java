976347ca62ab25db72957f12bf52d664cd6e080c
Steven Mpawulo <stevenmpawulo@gmail.com>
2024-02-20 11:26:57 +0300

    adds constructors, getters and setters

--- src/main/java/com/example/devtoclone/models/Article.java
+++ src/main/java/com/example/devtoclone/models/Article.java
@@ -1,6 +1,8 @@
 package com.example.devtoclone.models;
 
 import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.util.Date;
 import java.util.List;
@@ -10,10 +12,34 @@ public class Article {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    protected Article() {}
+
+    public Article(String imageUrl, Date createdOn, Date updatedOn, String title, String content, String readTime, boolean isBookmarked, User user, List<Comment> comments, List<Reaction> reactions) {
+        this.imageUrl = imageUrl;
+        this.createdOn = createdOn;
+        this.updatedOn = updatedOn;
+        this.title = title;
+        this.content = content;
+        this.readTime = readTime;
+        this.isBookmarked = isBookmarked;
+        this.user = user;
+        this.comments = comments;
+        this.reactions = reactions;
+    }
+
     private String imageUrl;
+    @CreationTimestamp
     private Date createdOn;
+    @UpdateTimestamp
     private Date updatedOn;
+    @Column(
+            nullable = false
+    )
     private String title;
+    @Column(
+            nullable = false
+    )
     private String content;
     private String readTime;
     private boolean isBookmarked;
@@ -21,4 +47,95 @@ public class Article {
     private User user;
     @OneToMany
     private List<Comment> comments;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public Date getCreatedOn() {
+        return createdOn;
+    }
+
+    public void setCreatedOn(Date createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    public Date getUpdatedOn() {
+        return updatedOn;
+    }
+
+    public void setUpdatedOn(Date updatedOn) {
+        this.updatedOn = updatedOn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getReadTime() {
+        return readTime;
+    }
+
+    public void setReadTime(String readTime) {
+        this.readTime = readTime;
+    }
+
+    public boolean isBookmarked() {
+        return isBookmarked;
+    }
+
+    public void setBookmarked(boolean bookmarked) {
+        isBookmarked = bookmarked;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
+    }
+
+    public List<Reaction> getReactions() {
+        return reactions;
+    }
+
+    public void setReactions(List<Reaction> reactions) {
+        this.reactions = reactions;
+    }
+
+    @OneToMany
+    private List<Reaction> reactions;
 }
