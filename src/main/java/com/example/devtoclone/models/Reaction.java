8c483c61e6e22f374915a804e9ce435b1ab02fd0
Steven Mpawulo <stevenmpawulo@gmail.com>
2024-02-20 12:05:25 +0300

    works on reaction model

--- src/main/java/com/example/devtoclone/models/Reaction.java
+++ src/main/java/com/example/devtoclone/models/Reaction.java
@@ -1,13 +1,47 @@
 package com.example.devtoclone.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class Reaction {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    protected Reaction() {
+    }
+
+    public Reaction(Article article, String content) {
+        this.article = article;
+        this.content = content;
+    }
+
+    @ManyToOne
+    private Article article;
+
+    private String content;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Article getArticle() {
+        return article;
+    }
+
+    public void setArticle(Article article) {
+        this.article = article;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
