0ae91c2c5f0d13da6fb9b57e5324e2ebea9a4a3d
Steven Mpawulo <stevenmpawulo@gmail.com>
2024-02-20 11:20:00 +0300

    works on article model and adds comments relationship

--- src/main/java/com/example/devtoclone/models/Article.java
+++ src/main/java/com/example/devtoclone/models/Article.java
@@ -1,13 +1,24 @@
 package com.example.devtoclone.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.util.Date;
+import java.util.List;
 
 @Entity
 public class Article {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    private String imageUrl;
+    private Date createdOn;
+    private Date updatedOn;
+    private String title;
+    private String content;
+    private String readTime;
+    private boolean isBookmarked;
+    @ManyToOne
+    private User user;
+    @OneToMany
+    private List<Comment> comments;
 }
---
+++ src/main/java/com/example/devtoclone/models/Comment.java
@@ -0,0 +1,13 @@
+package com.example.devtoclone.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+}
