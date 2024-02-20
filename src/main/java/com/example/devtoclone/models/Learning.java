691bddded69b9f05cf62f1bb594bfedf1a4050e0
Steven Mpawulo <stevenmpawulo@gmail.com>
2024-02-20 12:02:39 +0300

    works on learning controller

--- src/main/java/com/example/devtoclone/models/Learning.java
+++ src/main/java/com/example/devtoclone/models/Learning.java
@@ -1,13 +1,48 @@
 package com.example.devtoclone.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class Learning {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
+
+    protected Learning() {
+    }
+
+    public Learning(User user, String content) {
+        this.user = user;
+        this.content = content;
+    }
+
+    @ManyToOne
+    private User user;
+
+    @Column(nullable = false)
+    private String content;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
