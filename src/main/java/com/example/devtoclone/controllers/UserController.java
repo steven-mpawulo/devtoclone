2088e0ffe0d3014ca10979a9af2d75d4825e9039
Steven Mpawulo <stevenmpawulo@gmail.com>
2024-02-20 10:59:53 +0300

    works on getting all users

--- src/main/java/com/example/devtoclone/controllers/UserController.java
+++ src/main/java/com/example/devtoclone/controllers/UserController.java
@@ -1,9 +1,23 @@
 package com.example.devtoclone.controllers;
 
+import com.example.devtoclone.models.User;
+import com.example.devtoclone.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
 public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/users")
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
 }
