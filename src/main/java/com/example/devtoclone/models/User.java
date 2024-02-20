a7e95e2ed8c0a63c0b7ccbff4c0acc9a8e00485e
Steven Mpawulo <stevenmpawulo@gmail.com>
2024-02-20 09:48:54 +0300

    starts work on user model and adds article relationship

---
+++ src/main/java/com/example/devtoclone/models/User.java
@@ -0,0 +1,39 @@
+package com.example.devtoclone.models;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String profilePicUrl;
+    private String location;
+    private String education;
+    private String briefDescription;
+    private String work;
+    @OneToMany(mappedBy = "user")
+    private List<Article> articles;
+
+
+    @CreationTimestamp
+    private Date createdOn;
+
+    @UpdateTimestamp
+    private Date updatedOn;
+
+
+
+}
