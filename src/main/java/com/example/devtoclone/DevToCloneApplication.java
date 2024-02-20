107c474994bec97ab8bf3a18de1d82f773143dff
Steven Mpawulo <stevenmpawulo@gmail.com>
2024-02-20 09:10:30 +0300

    initial commit

---
+++ src/main/java/com/example/devtoclone/DevToCloneApplication.java
@@ -0,0 +1,13 @@
+package com.example.devtoclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevToCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevToCloneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/devtoclone/DevToCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devtoclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevToCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
