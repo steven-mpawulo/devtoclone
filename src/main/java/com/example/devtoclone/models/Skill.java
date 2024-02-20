c74a7cbe5cf21aa1a65eaaec37572d2996371074
Steven Mpawulo <stevenmpawulo@gmail.com>
2024-02-20 12:10:34 +0300

    works on skill model

--- src/main/java/com/example/devtoclone/models/Skill.java
+++ src/main/java/com/example/devtoclone/models/Skill.java
@@ -1,13 +1,44 @@
 package com.example.devtoclone.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class Skill {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    protected Skill() {}
+    public Skill(User user, String content) {
+        this.user = user;
+        this.content = content;
+    }
+
+    @ManyToOne
+    private User user;
+    @Column(nullable = false)
+    private String content;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
