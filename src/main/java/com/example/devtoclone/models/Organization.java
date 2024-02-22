500e3542cb68bbe32dfb38f7cf7c249a96ecabb8
Steven Mpawulo <stevenmpawulo@gmail.com>
2024-02-22 09:05:36 +0300

    works on organization model

--- src/main/java/com/example/devtoclone/models/Organization.java
+++ src/main/java/com/example/devtoclone/models/Organization.java
@@ -2,10 +2,43 @@ package com.example.devtoclone.models;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 @Entity
 @Table(schema = "public")
 public class Organization {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    private String organizationName;
+    @OneToMany
+    private List<User> users;
+
+    public String getOrganizationName() {
+        return organizationName;
+    }
+
+    protected Organization() {
+    }
+
+    public Organization(String organizationName, List<User> users) {
+        this.organizationName = organizationName;
+        this.users = users;
+    }
+
+    public void setOrganizationName(String organizationName) {
+        this.organizationName = organizationName;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+
+
+
 }
