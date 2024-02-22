bde948eec6c0ecb57ab76dc1e4863cd274ee287d
Steven Mpawulo <stevenmpawulo@gmail.com>
2024-02-22 14:08:09 +0300

    adds repository

--- src/main/java/com/example/devtoclone/repositories/OrganizationRepository.java
+++ src/main/java/com/example/devtoclone/repositories/OrganizationRepository.java
@@ -2,6 +2,8 @@ package com.example.devtoclone.repositories;
 
 import com.example.devtoclone.models.Organization;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OrganizationRepository extends JpaRepository<Organization, Long> {
 }
