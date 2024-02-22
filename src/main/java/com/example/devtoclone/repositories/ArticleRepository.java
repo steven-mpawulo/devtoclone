22b6cd68b04b2981371f9ffbdb92e3d3d16ae76e
Steven Mpawulo <stevenmpawulo@gmail.com>
2024-02-22 13:59:02 +0300

    adds repository annotation

--- src/main/java/com/example/devtoclone/repositories/ArticleRepository.java
+++ src/main/java/com/example/devtoclone/repositories/ArticleRepository.java
@@ -2,7 +2,9 @@ package com.example.devtoclone.repositories;
 
 import com.example.devtoclone.models.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 
 }
--- src/main/java/com/example/devtoclone/repositories/CommentRepository.java
+++ src/main/java/com/example/devtoclone/repositories/CommentRepository.java
@@ -2,9 +2,11 @@ package com.example.devtoclone.repositories;
 
 import com.example.devtoclone.models.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findAllByArticle_IdOrderByCreatedOnDesc(Long articleId);
 }
